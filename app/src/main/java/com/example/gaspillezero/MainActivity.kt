9131f7608dcbac9db34e1c8937523c5f51be6327
Audric <audric.ligtas@gmail.com>
2023-11-02 12:43:49 -0400

    Ajoutage nav pas vraiment

--- app/src/main/java/com/example/gaspillezero/MainActivity.kt
+++ app/src/main/java/com/example/gaspillezero/MainActivity.kt
@@ -2,12 +2,16 @@ package com.example.gaspillezero
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import com.example.gaspillezero.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView3) as NavHostFragment
+        val navController = navHostFragment.navController
         setContentView(R.layout.activity_main)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
