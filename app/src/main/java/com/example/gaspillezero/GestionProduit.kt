bbc9ff3bd66d402f9a2f93c516986a7d16585e13
Montplaisir Samuel <1977223@crosemont.qc.ca>
2023-11-03 11:52:38 -0400

    Fonction supprimer + Amélioration Allignement colonnes

--- app/src/main/java/com/example/gaspillezero/GestionProduit.kt
+++ app/src/main/java/com/example/gaspillezero/GestionProduit.kt
@@ -6,19 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
-import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.ListView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 
 class GestionProduit : Fragment() {
 
     private lateinit var listView: ListView
     private lateinit var customAdapter: CustomAdapter
 
-    // Données fictives
-    private val dataList = listOf(
+    //MutableList
+    private val dataList = mutableListOf(
         Produit("Fraises", "2024-01-01", 5, 3.99),
         Produit("Saumon", "2024-05-21", 10, 1.99),
         Produit("Spaghetti", "2023-12-31", 8, 2.49)
@@ -32,15 +31,10 @@ class GestionProduit : Fragment() {
         listView = view.findViewById(R.id.lvProduits)
         customAdapter = CustomAdapter(requireContext(), dataList)
         listView.adapter = customAdapter
-        val ButtonAccueil = view.findViewById<ImageButton>(R.id.Accueil)
-        ButtonAccueil.setOnClickListener{
-            findNavController().navigate(R.id.action_gestionProduit_to_epicerie_accueil)
-        }
         return view
     }
 
-    // Adapter pour le ListView
-    private class CustomAdapter(private val context: Context, private val dataList: List<Produit>) : BaseAdapter() {
+    private inner class CustomAdapter(private val context: Context, private val dataList: MutableList<Produit>) : BaseAdapter() {
         override fun getCount(): Int = dataList.size
         override fun getItem(position: Int): Any = dataList[position]
         override fun getItemId(position: Int): Long = position.toLong()
@@ -54,11 +48,22 @@ class GestionProduit : Fragment() {
             view.findViewById<TextView>(R.id.tvQuantite).text = produit.quantite.toString()
             view.findViewById<TextView>(R.id.tvPrix).text = String.format("%.2f $", produit.prix)
 
+            //gestion du clic sur l'icône de suppression
+            view.findViewById<ImageView>(R.id.image_delete).setOnClickListener {
+                removeItem(position)
+            }
+
             return view
+        }
+
+        //fonction supprimer
+        fun removeItem(position: Int) {
+            dataList.removeAt(position)
+            notifyDataSetChanged() //actualiser la liste
         }
     }
 
-    // Données de produit
+
     data class Produit(
         val nom: String,
         val dateExpiration: String,
