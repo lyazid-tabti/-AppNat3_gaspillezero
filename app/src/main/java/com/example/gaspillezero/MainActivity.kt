b38acbacfdb36c7d25da045e3a6143d0f87716b1
Jean_Gabriel <2016069@crosemont.qc.ca>
2023-11-02 22:12:11 -0400

    test

--- app/src/main/java/com/example/gaspillezero/MainActivity.kt
+++ app/src/main/java/com/example/gaspillezero/MainActivity.kt
@@ -9,10 +9,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
+
+            supportFragmentManager.beginTransaction().replace(R.id.container, fragment_epecerie()).commit()
+
         }
     }
-}
--- app/src/main/java/com/example/gaspillezero/fragment_epecerie.kt
+++ app/src/main/java/com/example/gaspillezero/fragment_epecerie.kt
@@ -20,47 +20,25 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class fragment_epecerie : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-    lateinit var btnpage2: Button
-    lateinit var navControl : NavController
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_epecerie, container, false)
+        val view = inflater.inflate(R.layout.fragment_epecerie, container, false)
 
+        val nextBtn : Button = view.findViewById(R.id.btnRetour)
+        nextBtn.setOnClickListener{
+            val fragment = fragment_detail()
+            val transcation = fragmentManager?.beginTransaction()
+            transcation?.replace(R.id.container,fragment)?.commit()
+        }
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment fragment_epecerie.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            fragment_epecerie().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
+
+
 }
